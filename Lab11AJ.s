24a421575a3b056ad7dc84e4e590fe8e60d888fb
Josrodjr <Josrodjr@gmail.com>
2017-05-04 08:27:12 -0600

    listo
    listo

--- Lab11AJ.s
+++ Lab11AJ.s
@@ -10,7 +10,6 @@
 .text
 .align 2
 .global main
-.type main,%function
 main:
 	@utilizando la biblioteca GPIO (gpio0_2.s)
 	bl GetGpioAddress @solo se llama una vez
@@ -35,20 +34,22 @@ main:
 	mov r1,#1
 	bl SetGpioFunction
 
-	bl wait
+	@Apagar GPIO 15
+	mov r0,#15
+	mov r1,#0
+	bl SetGpio
 
 	@Apagar GPIO 18
 	mov r0,#18
-	mov r1,#0
+	mov r1,#1
 	bl SetGpio
 
 	@Apagar GPIO 23
 	mov r0,#23
-	mov r1,#0
+	mov r1,#1
 	bl SetGpio
 
 	bl wait
-
 
 loop:
 	ldr r6, =myloc
@@ -56,11 +57,13 @@ loop:
 	ldr r5,[r0,#0x34]	@Direccion r0+0x34:lee en r5 estado de puertos de entrada
 	mov r7,#1
 	lsl r7,#14
-	and r5,r7 		@se revisa el bit 14 (puerto de entrada)
+	and r7,r5,r7 		@se revisa el bit 14 (puerto de entrada)
 
 	@Si el boton esta en alto (1), fue presionado y enciende GPIO 15
-	cmp r5,#0
-	beq paso2
+	teq r7,#0
+
+	bne paso2
+	@bl wait
 	b loop
 
 paso2:
@@ -69,9 +72,9 @@ paso2:
 	ldr r5,[r0,#0x34]	@Direccion r0+0x34:lee en r5 estado de puertos de entrada
 	mov r7,#1
 	lsl r7,#15
-	and r5,r7 		@se revisa el bit 15 (puerto de salida)
+	and r7,r5,r7 		@se revisa el bit 15 (puerto de salida)
 
-	cmp r5, #0
+	teq r7, #0
 	beq salida1
 	bne paso3
 	b loop
@@ -81,10 +84,10 @@ paso3:
  	ldr r0, [r6]		@ obtener direccion de la memoria virtual 
 	ldr r5,[r0,#0x34]	@Direccion r0+0x34:lee en r5 estado de puertos de entrada
 	mov r7,#1
-	lsl r7,#18
-	and r5,r7 		@se revisa el bit 18 (puerto de salida)
+	lsl r7,#23
+	and r7,r5,r7 		@se revisa el bit 18 (puerto de salida)
 
-	cmp r5, #0
+	teq r7, #0
 	beq salida2
 	bne paso4
 	b loop
@@ -94,41 +97,58 @@ paso4:
  	ldr r0, [r6]		@ obtener direccion de la memoria virtual 
 	ldr r5,[r0,#0x34]	@Direccion r0+0x34:lee en r5 estado de puertos de entrada
 	mov r7,#1
-	lsl r7,#23
-	and r5,r7 		@se revisa el bit 23 (puerto de salida)
+	lsl r7,#18
+	and r7,r5,r7 		@se revisa el bit 23 (puerto de salida)
 
-	cmp r5, #0
+	teq r7, #0
 	beq salida3
 	bne loop
 	b loop
 
 salida1:
-	movne r0,#15		@instrucciones para encender GPIO 15
-	movne r1,#1
-	blne SetGpio
+	mov r0,#15		@instrucciones para encender GPIO 15
+	mov r1,#1
+	bl SetGpio
+
+	mov r0,#23		@instrucciones para encender GPIO 15
+	mov r1,#0
+	bl SetGpio
 		
 	b loop
 
 salida2:
-	movne r0,#18		@instrucciones para encender GPIO 18
-	movne r1,#1
-	blne SetGpio
+	mov r0,#23		@instrucciones para encender GPIO 18
+	mov r1,#1
+	bl SetGpio
+
+	mov r0,#18		@instrucciones para encender GPIO 18
+	mov r1,#0
+	bl SetGpio
 		
 	b loop
 
 salida3:
-	movne r0,#23		@instrucciones para encender GPIO 23
-	movne r1,#1
-	blne SetGpio
+	mov r0,#18		@instrucciones para encender GPIO 23
+	mov r1,#1
+	bl SetGpio
+
+	mov r0,#15		@instrucciones para encender GPIO 23
+	mov r1,#0
+	bl SetGpio
 		
 	b loop
 
 wait:
-	mov r0, #0xF000000 @ big number
+ mov r0, #0x4000000 @ big number
+sleepLoop:
+ subs r0,#1
+ bne sleepLoop @ loop delay
+ mov pc,lr
 
-.data
-.align 2
+ .data
+ .align 2
 .global myloc
 myloc: .word 0
 
  .end
+
