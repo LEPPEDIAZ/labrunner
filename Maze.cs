43a57e8fcf70ae0c91ac6b4160bd0ae3fde6c81c
LEPPEDIAZ <LEPPEDIAZ@users.noreply.github.com>
2017-05-16 02:41:32 -0300

    Update Maze.cs

--- Maze.cs
+++ Maze.cs
@@ -8,17 +8,11 @@ public class Maze {
 
     Cell[,] mazeCells;
 
-    /// <summary>
-    /// Gets the number of columns in this maze
-    /// </summary>
+    /// obtiene un numero de la columna del laberinto
     public int Columns { get; private set; }
-    /// <summary>
-    /// Gets the number of rows in this maze
-    /// </summary>
+    /// pone un get rows del laberinto
     public int Rows { get; private set; }
-    /// <summary>
-    /// Gets the scale in which this maze is drawn
-    /// </summary>
+    /// toma la escala donde se saca el laberinto
     public float Scale { get; private set; }
 
     public Maze(int rows, int columns, float scale)
@@ -37,10 +31,8 @@ public class Maze {
         drawing(this, new MazeDrawEventArgs(runner, wall, Scale));
     }
     /// <summary>
-    /// Gets all the cells in this maze as a simple linear collection.
-    /// It simply "flattens" the maze from a multi-dimensional array to a one-dimensional list
-    /// </summary>
-    /// <returns></returns>
+    /// Gets all the cells en una linea en una coneccion lineal
+  
     IEnumerable<Cell> GetAllCells()
     {
         List<Cell> allCells = new List<Cell>(Rows * Columns);
@@ -48,43 +40,29 @@ public class Maze {
         {
             allCells.Add(cell);
         }
-        //for (int i = 0; i < Rows; i++)
-        //{
-        //    for (int j = 0; j < Columns; j++)
-        //    {
-        //        allCells.Add(mazeCells[i, j]);
-        //    }
-        //}
+     
         return allCells;
     }
     /// <summary>
-    /// Gets the cell at the specified row and column
-    /// </summary>
-    /// <param name="row"></param>
-    /// <param name="column"></param>
-    /// <returns></returns>
+    /// Gets the cell de una row o column
+  
     public Cell GetCell(int row, int column)
     {
         return GetAllCells().FirstOrDefault(c => c.Row == row && c.Column == column);
     }
-    /// <summary>
-    /// Randomly picks one cell from a collection of cells
-    /// </summary>
-    /// <param name="cells">An optional collection to use. if null, all cells in the maze are considered.</param>
-    /// <returns></returns>
+ 
     Cell GetRandomCell(IEnumerable<Cell> cells = null)
     {
         if (cells == null)
             cells = GetAllCells();
 
-        //Random random = new Random();
-        //CryptoRandom random = new CryptoRandom();
+        
         int randomIndex = UnityEngine.Random.Range(0, cells.Count());//random.Next(cells.Count());
         return cells.ToList()[randomIndex];
     }
-    /// <summary>
-    /// Fills all the squares in the maze with actual cells
-    /// </summary>
+ 
+    /// llena todo en el laberinto con cells
+>
     void Init()
     {
         for (int i = 0; i < Rows; i++)
@@ -98,14 +76,10 @@ public class Maze {
                         Row = i,
                     };
             }
-        }
-
-        //make the last added cell the end cell
+        }        
         mazeCells[Rows - 1, Columns - 1].ExitCell = true;
     }
-    /// <summary>
-    /// This is the method that actually defines the paths of the maze.
-    /// </summary>
+   //define el camino del laberinto
     public void Initialize()
     {
         Init();
@@ -159,41 +133,4 @@ public class MazeDrawEventArgs : EventArgs
     public GameObject Wall { get; private set; }
 }
 
-///// <summary>
-///// This class is AWESOME.
-///// This truly random num gen is what makes my maze look like a real maze.
-///// http://eimagine.com/how-to-generate-better-random-numbers-in-c-net-2/
-///// </summary>
-//public class CryptoRandom : RandomNumberGenerator
-//{
-//    public static RandomNumberGenerator r;
 
-//    public CryptoRandom()
-//    {
-//        r = RandomNumberGenerator.Create();
-//    }
-
-//    public override void GetBytes(byte[] data)
-//    {
-//        r.GetBytes(data);
-//    }
-
-//    public double NextDouble()
-//    {
-//        byte[] b = new byte[4];
-//        r.GetBytes(b);
-//        return (double)BitConverter.ToUInt32(b, 0) / UInt32.MaxValue;
-//    }
-//    public int Next(int minValue, int maxValue)
-//    {
-//        return (int)Math.Round(NextDouble() * (maxValue - minValue - 1)) + minValue;
-//    }
-//    public int Next()
-//    {
-//        return Next(0, Int32.MaxValue);
-//    }
-//    public int Next(int maxValue)
-//    {
-//        return Next(0, maxValue);
-//    }
-//}
